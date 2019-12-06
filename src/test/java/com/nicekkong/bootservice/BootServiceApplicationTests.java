60cc06ce941a071a8b20cb6f6587d1fe391d2e5b
공병일(Byung-il, Kong)/OCB개발팀/SKP <bikong@sk.com>
2019-12-06 10:29:13 +0900

    first commit

---
+++ src/test/java/com/nicekkong/bootservice/BootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicekkong.bootservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
